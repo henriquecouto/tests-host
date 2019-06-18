70bdab02bd6998f7d9c4a6ca5318c7ff72a56fe0
Henrique Couto <henrique.coutu@live.com>
2019-06-18 14:42:07 -0300

    parâmetros script

--- src/script.sh
+++ src/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in $(seq 517000 518000)
+for i in $(seq $1 $2)
 do
   curl -A "Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_3 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) CriOS/28.0.1500.12 Mobile/10B329 Safari/8536.25" -IL "m.diarioonline.com.br/_/$i"
 done
